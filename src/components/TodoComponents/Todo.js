c620dbfadab3b0dc903c06200d8e8ecdbc6db3b7
zachary-peasley <zpman95@hotmail.com>
2019-09-02 12:13:58 -0500

    Added props in Todo components

---
+++ src/components/TodoComponents/Todo.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Todo = props => {
+    console.log(props)
+    return (
+        <>
+        <p>{props.item.task}</p>
+        </>
+    )
+}
+
+export default Todo;
